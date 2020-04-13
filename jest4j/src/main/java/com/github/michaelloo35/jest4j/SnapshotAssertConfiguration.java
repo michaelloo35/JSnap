9e6ec35aa90e654614004766a7532b46a1680d9b
michaelloo35 <michaelloo35@gmail.com>
2020-04-13 20:10:31 +0200

    + Added example snapshot + (optimization) Enabled afterburner module for jackson ~ Improved exception messages + Improved style of timestamp serialization

--- jest4j/src/main/java/com/github/michaelloo35/jest4j/SnapshotAssertConfiguration.java
+++ jest4j/src/main/java/com/github/michaelloo35/jest4j/SnapshotAssertConfiguration.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.afterburner.AfterburnerModule;
 import com.flipkart.zjsonpatch.DiffFlags;
 
 import java.util.EnumSet;
@@ -12,22 +13,25 @@ import static java.lang.ThreadLocal.withInitial;
 
 public class SnapshotAssertConfiguration {
 
-    static EnumSet<DiffFlags> DIFF_FLAGS = EnumSet.of(DiffFlags.ADD_ORIGINAL_VALUE_ON_REPLACE);
-    static String snapshotGenerationLocation = "./";
-
     private static final ThreadLocal<ObjectMapper> om = withInitial(() -> {
-        ObjectMapper objectMapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
+        ObjectMapper objectMapper = new ObjectMapper()
+                .enable(SerializationFeature.INDENT_OUTPUT)
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
         objectMapper.registerModule(new Jdk8Module());
         objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.registerModule(new AfterburnerModule());
         return objectMapper;
     });
+    static EnumSet<DiffFlags> DIFF_FLAGS = EnumSet.of(DiffFlags.ADD_ORIGINAL_VALUE_ON_REPLACE);
+    static String snapshotGenerationAbsolutePath = System.getProperty("user.dir");
 
     public static ObjectMapper getObjectMapper() {
         return om.get();
     }
 
-    public static void setSnapshotGenerationLocation(String path) {
-        SnapshotAssertConfiguration.snapshotGenerationLocation = path;
+    public static void setSnapshotGenerationAbsolutePath(String path) {
+        SnapshotAssertConfiguration.snapshotGenerationAbsolutePath = path;
     }
 }
 
--- jest4j/src/main/java/com/github/michaelloo35/jest4j/SnapshotMatcher.java
+++ jest4j/src/main/java/com/github/michaelloo35/jest4j/SnapshotMatcher.java
@@ -6,65 +6,68 @@ import com.flipkart.zjsonpatch.JsonDiff;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Paths;
 
 import static com.github.michaelloo35.jest4j.SnapshotAssertConfiguration.DIFF_FLAGS;
 import static com.github.michaelloo35.jest4j.SnapshotAssertConfiguration.getObjectMapper;
-import static com.github.michaelloo35.jest4j.SnapshotAssertConfiguration.snapshotGenerationLocation;
+import static com.github.michaelloo35.jest4j.SnapshotAssertConfiguration.snapshotGenerationAbsolutePath;
 
 public class SnapshotMatcher implements MatchingStep {
 
     private static final String EMPTY_ARRAY_JSON = "[]";
-    public static final String RESOURCES_RELATIVE_SNAPSHOTS_DIRECTORY = "snapshots";
     private final Object actual;
-
-    private SnapshotMatcher(Object actual) {
-        this.actual = actual;
-    }
 
     public static MatchingStep expect(Object actual) {
         return new SnapshotMatcher(actual);
     }
 
-    @Override
-    public void toMatchSnapshot(String uniqueSnapshotFileName) {
-        String snapshotFileName = uniqueSnapshotFileName + ".json";
-        String resourcesRelativeSnapshotPath = RESOURCES_RELATIVE_SNAPSHOTS_DIRECTORY + '/' + snapshotFileName;
+    private SnapshotMatcher(Object actual) {
+        this.actual = actual;
+    }
 
-        URL snapshotResource = getClass().getClassLoader().getResource(resourcesRelativeSnapshotPath);
+    @Override
+    public void toMatchSnapshot(String resourcesRelativeSnapshotPath) {
+        String resourcesRelativeSnapshotPathWithExtension =
+                resourcesRelativeSnapshotPath.endsWith(".json")
+                        ? resourcesRelativeSnapshotPath
+                        : resourcesRelativeSnapshotPath + ".json";
+
+        String snapshotFileName = Paths.get(resourcesRelativeSnapshotPathWithExtension).getFileName().toString();
+
+        URL snapshotResource = getClass().getClassLoader().getResource(resourcesRelativeSnapshotPathWithExtension);
         if (snapshotResource != null) {
-            compareWithSnapshot(snapshotFileName, snapshotResource);
+            compareWithExistingSnapshot(snapshotFileName, snapshotResource);
         } else {
-            createSnapshotFile(snapshotFileName);
+            createSnapshotFile(snapshotFileName, resourcesRelativeSnapshotPathWithExtension);
         }
     }
 
-    private void compareWithSnapshot(String snapshotFileName, URL snapshotResource) {
+    private void compareWithExistingSnapshot(String snapshotFileName, URL snapshotResource) {
         try {
-            JsonNode expected = null;
-            expected = getObjectMapper().readTree(snapshotResource);
-
+            JsonNode expected = getObjectMapper().readTree(snapshotResource);
             JsonNode actual = getObjectMapper().readTree(getObjectMapper().writeValueAsString(this.actual));
             JsonNode differences = JsonDiff.asJson(expected, actual, DIFF_FLAGS);
 
             if (!differences.equals(getObjectMapper().readTree(EMPTY_ARRAY_JSON))) {
                 throw new AssertionFailureException(
-                        "\nActual does not match snapshot\n" + snapshotFileName +
-                                "\n\nDifferences:\n" + getObjectMapper().writeValueAsString(differences));
+                        "\nActual does not match expected\n" + snapshotFileName +
+                                "\n\nDifferences are:\n" + getObjectMapper().writeValueAsString(differences));
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private void createSnapshotFile(String snapshotFileName) {
+    private void createSnapshotFile(String snapshotFileName, String resourcesRelativeSnapshotFilePathWithExtension) {
         try {
-            File generatedSnapshot = new File(snapshotGenerationLocation + snapshotFileName);
+            File generatedSnapshot = new File(snapshotGenerationAbsolutePath, snapshotFileName);
             getObjectMapper().writeValue(generatedSnapshot, actual);
 
             throw new FailAfterInitialSnapshotGenerationException(
                     "\n\nCreated snapshot under:\n" + generatedSnapshot.getAbsolutePath() +
-                            "\n\nPlease verify the results and move them to:\n"
-                            + RESOURCES_RELATIVE_SNAPSHOTS_DIRECTORY + '/' + snapshotFileName);
+                            "\n\nPlease verify contents of snapshot and move it to path:\n"
+                            + resourcesRelativeSnapshotFilePathWithExtension +
+                            "\nwhich is relative to your resources directory.");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
